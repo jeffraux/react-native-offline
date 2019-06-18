17aa25eff620aad425a49bf9a3034d0af65f509a
Jefferson Aux <jeff@tasgroup.com.au>
2019-06-18 08:34:28 +0800

    react-native-offline legacy

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 module.exports = {
-  get NetworkProvider() {
-    return require('./components/NetworkProvider').default;
-  },
-  get ReduxNetworkProvider() {
-    return require('./components/ReduxNetworkProvider').default;
-  },
-  get NetworkConsumer() {
-    return require('./components/NetworkConsumer').default;
-  },
   get reducer() {
-    return require('./redux/reducer').default;
-  },
-  get createNetworkMiddleware() {
-    return require('./redux/createNetworkMiddleware').default;
-  },
-  get offlineActionTypes() {
-    return require('./redux/actionTypes').default;
-  },
-  get networkSaga() {
-    return require('./redux/sagas').default;
-  },
-  get checkInternetConnection() {
-    return require('./utils/checkInternetConnection').default;
+    return require('./reducer').default;
   },
   get withNetworkConnectivity() {
     return require('./withNetworkConnectivity').default;
+  },
+  get ConnectivityRenderer() {
+    return require('./ConnectivityRenderer').default;
+  },
+  get createNetworkMiddleware() {
+    return require('./createNetworkMiddleware').default;
+  },
+  get offlineActionTypes() {
+    return require('./actionTypes').default;
+  },
+  get networkEventsListenerSaga() {
+    return require('./sagas').default;
+  },
+  get checkInternetConnection() {
+    return require('./checkInternetConnection').default;
   },
 };
